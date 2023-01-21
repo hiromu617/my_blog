49f63ddc0a16fa7cb9fc0fa679443cfe2e064080
hiromu617 <hiromhiro617@gmail.com>
2023-01-22 00:06:59 +0900

    add editor

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,27 +1,47 @@
 import type { NextPage } from "next";
 import { useState } from "react";
 import { convertToHTMLString } from "@hiromu617/markdown-parser";
+import { Textarea, Container, Card, Switch, Stack } from "@mantine/core";
 
 const EditorPage: NextPage = () => {
+  const [isShowPreview, setIsShowPreview] = useState(false);
   const [markdown, setMarkdown] = useState("");
   const [html, setHtml] = useState("");
 
   return (
-    <div>
-      <textarea
-        value={markdown}
-        onChange={(e) => {
-          setMarkdown(e.target.value);
-          const html = convertToHTMLString(e.target.value);
-          setHtml(html);
-        }}
-      />
-      <div
-        dangerouslySetInnerHTML={{
-          __html: html,
-        }}
-      />
-    </div>
+    <Container my={100}>
+      <Stack style={{ width: 600, margin: "auto", minHeight: "1000px" }}>
+        <Switch
+          label="Show preview"
+          size="lg"
+          checked={isShowPreview}
+          onChange={(e) => {
+            setIsShowPreview(e.target.checked);
+            const html = convertToHTMLString(markdown);
+            setHtml(html);
+          }}
+        />
+        {isShowPreview ? (
+          <Card shadow="xs" p="sm" radius="md" withBorder>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: html,
+              }}
+            />
+          </Card>
+        ) : (
+          <Textarea
+            minRows={50}
+            autosize
+            value={markdown}
+            onChange={(e) => {
+              setMarkdown(e.target.value);
+            }}
+            style={{ width: "100%" }}
+          />
+        )}
+      </Stack>
+    </Container>
   );
 };
 
